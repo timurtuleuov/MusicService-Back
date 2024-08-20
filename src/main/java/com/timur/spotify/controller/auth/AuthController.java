93dde65df40ab38aaa4dd6da27f84ade47b47613
Timur Tuleuov <timmmur123@gmail.com>
2024-08-20 15:59:07 +0500

    update: auth controller

--- src/main/java/com/timur/spotify/controller/auth/AuthController.java
+++ src/main/java/com/timur/spotify/controller/auth/AuthController.java
@@ -1,4 +1,35 @@
 package com.timur.spotify.controller.auth;
 
+import com.timur.spotify.dto.JwtAuthenticationResponse;
+import com.timur.spotify.dto.SignInRequest;
+import com.timur.spotify.dto.SignUpRequest;
+import com.timur.spotify.service.auth.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/auth")
 public class AuthController {
+    private final AuthService authService;
+    private final ProducerService kafkaProducer;
+
+    public AuthController(AuthService authService, ProducerService kafkaProducer) {
+        this.authService = authService;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        JwtAuthenticationResponse response = authService.signUp(request);
+        this.kafkaProducer.sendMessage("User signed up: " + request.getUsername());
+        return response;
+    }
+
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        JwtAuthenticationResponse response = authService.signIn((request));
+        this.kafkaProducer.sendMessage("User signed in: " + request.getUsername());
+        return response;
+    }
 }
