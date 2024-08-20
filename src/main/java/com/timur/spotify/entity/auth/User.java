8b58308107c04ed4a25f73816163493c2b27ed50
Timur Tuleuov <timmmur123@gmail.com>
2024-08-20 15:05:15 +0500

    add: user entity

---
+++ src/main/java/com/timur/spotify/entity/auth/User.java
@@ -0,0 +1,25 @@
+package com.timur.spotify.entity.auth;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+}
