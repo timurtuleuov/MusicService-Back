e06766098ed5afe713ec77bc243951edafe5d935
Timur Tuleuov <timmmur123@gmail.com>
2024-08-20 15:33:06 +0500

    add: SignInRequest

--- src/main/java/com/timur/spotify/dto/SignUpRequest.java
+++ src/main/java/com/timur/spotify/dto/SignUpRequest.java
@@ -1,4 +1,30 @@
 package com.timur.spotify.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Запрос на регистрацию")
 public class SignUpRequest {
+    @Schema(description = "Имя пользователя", example = "Joh")
+    @Size(min = 3, max = 50, message = "Username must have from 3 to 50 symbols")
+    @NotBlank(message = "Имя пользователя не может быть пустыми")
+    private String username;
+
+    @Schema(description = "Адрес электронной почты", example = "jondoe@gmail.com")
+    @Size(min = 5, max = 50, message = "Адрес электронной почты должен содержать от 5 до 50 символов")
+    @NotBlank(message = "Адрес электронной почты не может быть пустыми")
+    @Email(message = "Email адрес должен быть в формате user@example.com")
+    private String email;
+
+    @Schema(description = "Пароль", example = "my_1secret1_password")
+    @Size(min = 3, max = 100, message = "Длина пароля должна быть не более 100 символов")
+    private String password;
 }
