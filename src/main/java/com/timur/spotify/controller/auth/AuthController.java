408b96c601e6036e1d44fbbcd7c86fc026fa9615
Timur Tuleuov <timmmur123@gmail.com>
2024-08-20 16:03:30 +0500

    update: Auth Controller; ProducerService

--- src/main/java/com/timur/spotify/controller/auth/AuthController.java
+++ src/main/java/com/timur/spotify/controller/auth/AuthController.java
@@ -4,10 +4,12 @@ import com.timur.spotify.dto.JwtAuthenticationResponse;
 import com.timur.spotify.dto.SignInRequest;
 import com.timur.spotify.dto.SignUpRequest;
 import com.timur.spotify.service.auth.AuthService;
+import com.timur.spotify.service.kafka.ProducerService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController("/auth")
 public class AuthController {
@@ -22,14 +24,14 @@ public class AuthController {
     @PostMapping("/sign-up")
     public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
         JwtAuthenticationResponse response = authService.signUp(request);
-        this.kafkaProducer.sendMessage("User signed up: " + request.getUsername());
+        this.kafkaProducer.sendMessage("auth-topic", "User signed up: " + request.getUsername());
         return response;
     }
 
     @PostMapping("/sign-in")
     public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
         JwtAuthenticationResponse response = authService.signIn((request));
-        this.kafkaProducer.sendMessage("User signed in: " + request.getUsername());
+        this.kafkaProducer.sendMessage("auth-topic", "User signed in: " + request.getUsername());
         return response;
     }
 }
--- src/main/java/com/timur/spotify/service/kafka/ProducerService.java
+++ src/main/java/com/timur/spotify/service/kafka/ProducerService.java
@@ -1,4 +1,16 @@
 package com.timur.spotify.service.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProducerService {
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+//    private static final String TOPIC = "auth-topic";
+
+    public void sendMessage(String TOPIC, String message){
+        kafkaTemplate.send(TOPIC, message);
+    }
 }
