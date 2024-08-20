0a151602af7b6125a4153df2a13769d6cc725a63
Timur Tuleuov <timmmur123@gmail.com>
2024-08-20 15:15:17 +0500

    add: User repository'

---
+++ src/main/java/com/timur/spotify/repository/auth/UserRepository.java
@@ -0,0 +1,12 @@
+package com.timur.spotify.repository.auth;
+
+import com.timur.spotify.entity.auth.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
