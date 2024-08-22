2f9e530376c09511a46824d6050c0428e369acc9
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 22:18:40 +0500

    Made DI of playlist service in controller

--- src/main/java/com/timur/spotify/controller/music/PlaylistController.java
+++ src/main/java/com/timur/spotify/controller/music/PlaylistController.java
@@ -1,9 +1,15 @@
 package com.timur.spotify.controller.music;
 
+import com.timur.spotify.service.music.PlaylistService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/playlist")
 public class PlaylistController {
+    private final PlaylistService playlistService;
+    
+    public PlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
 }
