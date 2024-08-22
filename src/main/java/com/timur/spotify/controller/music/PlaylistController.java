39aacfa2928130e187a847c645d3ed953b369a90
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 16:16:31 +0500

    Create Playlist controller

--- src/main/java/com/timur/spotify/controller/music/PlaylistController.java
+++ src/main/java/com/timur/spotify/controller/music/PlaylistController.java
@@ -1,2 +1,9 @@
-package com.timur.spotify.controller.music;public class PlaylistController {
+package com.timur.spotify.controller.music;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/playlist")
+public class PlaylistController {
 }
