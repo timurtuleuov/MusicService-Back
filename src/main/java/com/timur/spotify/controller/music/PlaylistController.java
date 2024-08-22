fc9915bc64fec7ab5c57f8000dcc206ca16cfbfd
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 22:19:29 +0500

    Made post mapping in playlist controller

--- src/main/java/com/timur/spotify/controller/music/PlaylistController.java
+++ src/main/java/com/timur/spotify/controller/music/PlaylistController.java
@@ -1,6 +1,11 @@
 package com.timur.spotify.controller.music;
 
+import com.timur.spotify.entity.music.Playlist;
 import com.timur.spotify.service.music.PlaylistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/playlist")
 public class PlaylistController {
     private final PlaylistService playlistService;
-    
+
     public PlaylistController(PlaylistService playlistService) {
         this.playlistService = playlistService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Playlist> createPlaylist(@RequestBody Playlist playlist) {
+        Playlist createdPlaylist = playlistService.createPlaylist(playlist);
+        return new ResponseEntity<>(createdPlaylist, HttpStatus.CREATED);
     }
 }
