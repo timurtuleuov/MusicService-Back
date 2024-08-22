ad5af72bd79555309fbdb55b35ebe6859712009c
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 23:03:28 +0500

    Made DI in Like Controller

--- src/main/java/com/timur/spotify/entity/music/LikeController.java
+++ src/main/java/com/timur/spotify/entity/music/LikeController.java
@@ -1,9 +1,22 @@
 package com.timur.spotify.entity.music;
 
+import com.timur.spotify.service.auth.UserService;
+import com.timur.spotify.service.music.LikeService;
+import com.timur.spotify.service.music.TrackService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tracks/{trackId}/likes")
 public class LikeController {
+
+    private final LikeService likeService;
+    private final TrackService trackService;
+    private final UserService userService;
+
+    public LikeController(LikeService likeService, TrackService trackService, UserService userService) {
+        this.likeService = likeService;
+        this.trackService = trackService;
+        this.userService = userService;
+    }
 }
