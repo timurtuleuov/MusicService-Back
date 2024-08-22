4badc689e34df92696f1aeb4ab6ddbe2b822b435
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 22:48:48 +0500

    Made DI in LikeService

--- src/main/java/com/timur/spotify/service/music/LikeService.java
+++ src/main/java/com/timur/spotify/service/music/LikeService.java
@@ -1,7 +1,13 @@
 package com.timur.spotify.service.music;
 
+import com.timur.spotify.repository.music.LikeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LikeService {
+    private final LikeRepository likeRepository;
+
+    public LikeService(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+    }
 }
