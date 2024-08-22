289e209ccb95e7a669da92b1127c81c843cdab09
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 23:02:49 +0500

    Add request mapping

--- src/main/java/com/timur/spotify/entity/music/LikeController.java
+++ src/main/java/com/timur/spotify/entity/music/LikeController.java
@@ -1,4 +1,9 @@
 package com.timur.spotify.entity.music;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tracks/{trackId}/likes")
 public class LikeController {
 }
