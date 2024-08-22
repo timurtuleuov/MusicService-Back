450fb575ee0ba05cf77b367b52706c2b3866a121
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 22:45:25 +0500

    Made interfaces of findByUserAndTrack, countByTrack, deleteByUserAndTrack in LikeRepository

--- src/main/java/com/timur/spotify/repository/music/LikeRepository.java
+++ src/main/java/com/timur/spotify/repository/music/LikeRepository.java
@@ -1,4 +1,16 @@
 package com.timur.spotify.repository.music;
 
-public interface LikeRepository {
+import com.timur.spotify.entity.auth.User;
+import com.timur.spotify.entity.music.Like;
+import com.timur.spotify.entity.music.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByUserAndTrack(User user, Track track);
+    long countByTrack(Track post);
+    void deleteByUserAndPost(User user, Track track);
 }
