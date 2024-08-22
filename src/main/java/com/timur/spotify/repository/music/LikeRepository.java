3ebedebbe4b0325a5cc938ece853e5bf0b72f214
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 22:46:13 +0500

    Create Like Service

--- src/main/java/com/timur/spotify/repository/music/LikeRepository.java
+++ src/main/java/com/timur/spotify/repository/music/LikeRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUserAndTrack(User user, Track track);
     long countByTrack(Track post);
-    void deleteByUserAndPost(User user, Track track);
+    void deleteByUserAndTrack(User user, Track track);
 }
