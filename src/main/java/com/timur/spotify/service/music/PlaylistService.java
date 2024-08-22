8455699ce2d6edbd991b23df0b24635e3aaae852
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 13:06:40 +0500

    Made getAllPlaylistByAuthorId method in Playlist Service

--- src/main/java/com/timur/spotify/service/music/PlaylistService.java
+++ src/main/java/com/timur/spotify/service/music/PlaylistService.java
@@ -1,5 +1,35 @@
 package com.timur.spotify.service.music;
 
+import com.timur.spotify.entity.music.Playlist;
+import com.timur.spotify.repository.music.PlaylistRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class PlaylistService {
+    private final PlaylistRepository playlistRepository;
+
+    public PlaylistService(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
+    public Playlist createPlaylist(Playlist playlist) {
+        return playlistRepository.save(playlist);
+    }
+
+    public List<Playlist> getAllPlaylistByAuthorId(Long authorId){
+        return playlistRepository.findAll().stream().filter(p -> p.getAuthor().getId().equals(authorId)).collect(Collectors.toList());
+    }
+
+    public Playlist updatePlaylist(Long id, Playlist playlist) {
+        if (playlistRepository.existsById(id)) {
+            playlist.setId(id);
+            return playlistRepository.save(playlist);
+        }
+        return null;
+    }
+
+
 }
