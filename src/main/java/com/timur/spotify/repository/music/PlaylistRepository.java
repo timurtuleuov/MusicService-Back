07293ae3ebcabb0dfdaa8e3f81f7495c32336097
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 12:40:40 +0500

    Update Playlist Repository, make extends JpaRepository

--- src/main/java/com/timur/spotify/repository/music/PlaylistRepository.java
+++ src/main/java/com/timur/spotify/repository/music/PlaylistRepository.java
@@ -1,4 +1,9 @@
 package com.timur.spotify.repository.music;
 
-public interface PlaylistRepository {
+import com.timur.spotify.entity.music.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 }
