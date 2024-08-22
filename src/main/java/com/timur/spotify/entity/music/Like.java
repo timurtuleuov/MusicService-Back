888576b896b186fb4b98c144be990dcede62c415
Timur Tuleuov <timmmur123@gmail.com>
2024-08-22 22:40:55 +0500

    Update like entity

--- src/main/java/com/timur/spotify/entity/music/Like.java
+++ src/main/java/com/timur/spotify/entity/music/Like.java
@@ -1,7 +1,25 @@
 package com.timur.spotify.entity.music;
 
-import jakarta.persistence.Entity;
+import com.timur.spotify.entity.auth.User;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Data
 @Entity
 public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "track_id", nullable = false)
+    private Track track;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime likedAt;
 }
