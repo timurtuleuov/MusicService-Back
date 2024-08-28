84d1aabfcefc9eb4ae6fa6edf3ed2b24b9f40837
Timur Tuleuov <timmmur123@gmail.com>
2024-08-28 15:28:10 +0500

    made playlist like entity

---
+++ src/main/java/com/timur/spotify/entity/music/PlaylistLike.java
@@ -0,0 +1,27 @@
+package com.timur.spotify.entity.music;
+
+import com.timur.spotify.entity.auth.User;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@DiscriminatorValue("PLAYLIST")
+public class PlaylistLike extends Like{
+
+    @ManyToOne
+    @JoinColumn(name = "playlist_id", nullable = false)
+    private Playlist playlist;
+
+    PlaylistLike(){}
+
+    PlaylistLike(User user, Playlist playlist){
+        super(user);
+        this.playlist = playlist;
+    }
+}
