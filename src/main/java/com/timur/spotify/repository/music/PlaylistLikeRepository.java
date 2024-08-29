3d3591630c929a93f735a3cdbc754b7a086d8e9a
Timur Tuleuov <timmmur123@gmail.com>
2024-08-29 23:45:38 +0500

    made playlistLike repository

--- src/main/java/com/timur/spotify/repository/music/PlaylistLikeRepository.java
+++ src/main/java/com/timur/spotify/repository/music/PlaylistLikeRepository.java
@@ -1,9 +1,17 @@
 package com.timur.spotify.repository.music;
 
+import com.timur.spotify.entity.auth.User;
+import com.timur.spotify.entity.music.Like;
+import com.timur.spotify.entity.music.Playlist;
 import com.timur.spotify.entity.music.PlaylistLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlaylistLikeRepository extends JpaRepository<PlaylistLike, Long> {
+    Optional<Like> findByUserAndPlaylist(User user, Playlist playlist);
+    long countByPlaylist(Playlist playlist);
+    void deleteByUserAndPlaylist(User user, Playlist playlist);
 }
